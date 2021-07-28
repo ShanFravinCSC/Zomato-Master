ef06306bf49476d3ad6a1130d05256ea27a2e728
ShanFravinCSC <shanfravincsc@gmail.com>
2021-07-28 19:57:35 +0530

    Schema Setup

---
+++ server/database/image/index.js
@@ -0,0 +1,11 @@
+import mongooose from "mongoose";
+
+const ImageSchema = new mongooose.Schema({
+    images: [
+        {
+            location: { type: String, required: true },
+        },
+    ],
+});
+
+export const ImageModel = mongooose.model("Images", ImageSchema);
