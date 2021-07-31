8cef49be996456dbebac2a8cad65cd65c3fd8e24
ShanFravinCSC <shanfravincsc@gmail.com>
2021-07-31 12:24:11 +0530

    Add statics and methods

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,7 +1,7 @@
 //Library
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+//import bcrypt from "bcryptjs";
+//import jwt from "jsonwebtoken";
 
 //Models
 import { UserModel } from "../../database/user"
@@ -11,7 +11,7 @@ const Router = express.Router();
 
 /*
 Route   /signup
-des     Signup with email and password
+des     Register New User
 params   none
 Access   public
 method   POST
@@ -19,37 +19,25 @@ method   POST
 
 Router.post("/signup", async(req, res) => {
     try{
-        const { email, password, fullname, phoneNumber} = req.body.credentials;
+        await UserModel.findByEmailAndPhone(req.body.credentials);
 
-        //check whether email exist
-        const checkUserByEmail = await UserModel.findOne({ email });
-        const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+       const newUser = await UserModel.create(req.body.credentials);
 
-        if(checkUserByEmail || checkUserByPhone){
-            return res.json({ error: "User already exist!" });
-        }
+        const token = newUser.generateJwtToken();
 
-        //hash the password
-        const bcryptSalt = await bcrypt.genSalt(8);
-
-        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
-
-        //save to DB
-        await UserModel.create({
-            ...req.body.credentials,
-            password: hashedPassword,
-        });
-
-        //generate JWT auth token
-        const token = jwt.sign({ user: { fullname, email } }, "ZomatoAPP");
-
-
-        //return
         return res.status(200).json({ token, status: "success"});
 
-    } catch(error){
+    }   catch(error){
         return res.status(500).json({ error: error.message });
     }
 });
+
+/*
+Route   /signin
+des     Sign in using email and password
+params   none
+Access   public
+method   POST
+*/
 
 export default Router;
