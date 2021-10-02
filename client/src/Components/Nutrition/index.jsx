a6908aa38a53fb3fc152ce31b16e0b423f0688c8
ShanFravinCSC <shanfravincsc@gmail.com>
2021-10-02 23:01:29 +0530

    added nutrition

---
+++ client/src/Components/Nutrition/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Components
+import NutritionCarousel from './NutritionCarousel';
+
+const Nutrition = () => {
+    return (
+        <div>
+            <NutritionCarousel />
+        </div>
+    )
+}
+
+export default Nutrition;
