265b0d0eb4123493c41416dbaf293223fac59868
ShanFravinCSC <shanfravincsc@gmail.com>
2021-10-05 11:21:40 +0530

    add Nutrition Card

---
+++ client/src/Components/Nutrition/NutritionCard.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import { BsStarFill, BsStar, BsStarHalf } from "react-icons/bs";
+
+const NutritionCard = (props) => {
+    return (
+        <>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/3">
+                <div className="w-full h-full bg-white rounded-2xl shadow-lg">
+                <div className={`w-full h-56 bg-${props.bg}-300`}>
+                    <img src={props.image}
+                    alt="suppliments" 
+                    className="w-full h-full"/>
+                </div>
+                <div className="p-3 flex flex-col gap-3">
+                    <div className="flex mt-2 items-center gap-3">
+                    <div className="h-4 w-4">
+                        <img
+                            src="https://www.pngkey.com/png/detail/261-2619381_chitr-veg-symbol-svg-veg-and-non-veg.png"
+                            alt="veg"
+                            className="w-full h-full"
+                        />
+                    </div>
+                    <ReactStars
+                        count={5}
+                        //onChange={ratingChanged}
+                        size={16}
+                        isHalf={true}
+                        value={3.5}
+                        emptyIcon={<BsStar />}
+                        halfIcon={<BsStarHalf />}
+                        fullIcon={<BsStarFill/>}
+                        activeColor="#ffd700"
+                    />
+                    <span className="text-gray-500">23</span>
+                </div>
+                    <h3 className="text-xl font-bold  text-gray-700">
+                        Burn - Weight Balance
+                    </h3>
+                    <p className="text-sm font-light text-gray-800">
+                        This formula with VFill™ technology will help improve metabolism and
+                        support fat burn.
+                    </p>
+                    <div className="mt-4">
+                        <hr/>
+                    </div>
+                    <div>
+                        <span>
+                        <s className="text-gray-400 font-light mr-3">₹600</s> <strong>₹320</strong>
+                        </span>
+                        <p>
+                            Monthly pack -30 capsules
+                        </p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default NutritionCard;
--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 //Components
 import NutritionHeroCarousel from './NutritionHeroCarousel';
 import NutritionCarousel from './NutritionCarousel';
+import NutritionCard from './NutritionCard';
 
 const Nutrition = () => {
     return (
@@ -10,6 +11,12 @@ const Nutrition = () => {
             <NutritionHeroCarousel />
             <div className="my-6">
                 <NutritionCarousel />
+            </div>
+            <div className="flex justify-between flex-wrap">
+                <NutritionCard bg="red" image="https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png?output-format=webp"/>
+                <NutritionCard bg="red" image="https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png?output-format=webp"/>
+                <NutritionCard bg="red" image="https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png?output-format=webp"/>
+                <NutritionCard bg="red" image="https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png?output-format=webp"/>
             </div>
         </div>
     )
